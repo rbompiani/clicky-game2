2170e9acce7e8a63c10968aeea45d69a4e66a55b
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-08-18 15:04:49 -0700

    added static files

---
+++ precache-manifest.c5ff304d8b47f020cbe570718d1fbc22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f658a9b0835d601dd2884dff8337078",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "dec6f37d03c164f6a01d",
+    "url": "/clicky-game/static/css/main.a2db7eb0.chunk.css"
+  },
+  {
+    "revision": "2c85d32550131ae6d666",
+    "url": "/clicky-game/static/js/2.d048d323.chunk.js"
+  },
+  {
+    "revision": "dec6f37d03c164f6a01d",
+    "url": "/clicky-game/static/js/main.3eff1853.chunk.js"
+  },
+  {
+    "revision": "e900226b6c6e02320770",
+    "url": "/clicky-game/static/js/runtime~main.98b6a1fe.js"
+  }
+]);
